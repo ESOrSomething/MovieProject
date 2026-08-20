5b133af969ffb6ed7bb09d1b877e1f3f8228d55a
ShaneFranklin-0 <shane.franklin@d11.org>
2026-08-20 13:16:36 -0600

    Initial changes, PIVs and getters added

--- MovieDB.java
+++ MovieDB.java
@@ -1,1 +1,33 @@
+public class MovieDB {
+    private String title;
+    private String leadActor;
+    private int releaseYear;
+    private String director;
 
+    public MovieDB(String t, String l, int r, String d) {
+        title = t;
+        leadActor = l;
+        releaseYear = r;
+        director = d;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLeadActor() {
+        return leadActor;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public static void readMoviesIntoAL() {
+        System.out.println("Placeholder");
+    }
+}
